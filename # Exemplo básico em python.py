15a1913ceede25e7ec727f50ddf55b11c1ca4177
Carlos Vinicius Rosa Galonete <vinnyr.gallonett@gmail.com>
2026-03-29 21:24:24 -0300

    Add files via upload

---
+++ # Exemplo básico em python.py
@@ -0,0 +1,13 @@
+# Exemplo básico em python
+
+# Entrada de dados
+
+nome =  input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+
+#Processamento
+ano_nascimento = 2007 - idade
+
+# Saída de dados
+print("Olá,",nome)
+print("Você nasceu em,",ano_nascimento)
---
+++ a = int(input()).py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+X = a + b
+print(f"X = {X}")
